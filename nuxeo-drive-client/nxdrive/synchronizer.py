12ad6d994d050f1019123baeb1b268165a3188b8
Jiakuan Wang <delight.wjk@gmail.com>
2013-12-13 08:51:43 +0800

    Merged master changes in

--- nuxeo-drive-client/nxdrive/synchronizer.py
+++ nuxeo-drive-client/nxdrive/synchronizer.py
@@ -1321,7 +1321,7 @@ class Synchronizer(object):
                 if self.should_stop_synchronization():
                     log.info("Stopping synchronization (pid=%d)", pid)
                     break
-                if (max_loops is not None and loop_count > max_loops):
+                if (max_loops is not None and loop_count >= max_loops):
                     log.info("Stopping synchronization after %d loops",
                              loop_count)
                     break
